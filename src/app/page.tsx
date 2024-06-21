1fa559fa5c275ee625647229c6faf3fc197a33a5
ManuLosta <mlostalom@gmail.com>
2024-06-21 20:36:26 -0300

    add about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,24 @@ import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen max-w-screen-md mx-auto flex-col items-center justify-between">
-			<section className="container flex flex-col justify-center h-screen px-4 gap-3">
+		<main className="flex max-w-screen-md mx-auto flex-col">
+			<section className="container flex flex-col justify-center px-4 gap-3 mt-40">
 				<h1 className="font-bold text-5xl">Manuel Lostaló</h1>
 				<p className="text-2xl">
-					<b>Desarrollador fullstack</b> y estudiante de Ingeniería en Informática
+					<b className="text-blue-900">Desarrollador fullstack</b> y estudiante de Ingeniería en Informática
 				</p>
 				<p>📍Buenos Aires, Argentina</p>
-				<div className="flex gap-2">
+				<div className="flex gap-2 flex-wrap">
 					<SocialLink icon={<FaGithub size={20} />} name="Github" url="https://github.com/ManuLosta" />
 					<SocialLink icon={<FaEnvelope size={20} />} name="mlostalom@gmail.com" url="mailto:mlostalom@gmail.com" />
 					<SocialLink icon={<FaLinkedin size={20} />} name="LinkeIn" url="https://www.linkedin.com/in/manuel-lostal%C3%B3-7b2b3a1ba/" />
 				</div>
 			</section>
-			<section>
-
+			<section className="container flex flex-col justify-center px-4 gap-3 mt-10">
+				<h1 className="font-bold text-2xl">Sobre mí</h1>
+				<p className="text-lg">
+					Me llamo <b className="text-blue-900">Manuel Lostaló</b>, soy un desarrollador apasionado por la tecnología y la creación de soluciones innovadoras. Con experiencia en múltiples lenguajes de programación y tecnologías, me dedico a desarrollar <b className="text-blue-900">aplicaciones aplicaciones web únicas</b>, enfocándome en el diseño y en la experiencia del usuario.
+				</p>
 			</section>
 		</main>
 	);
--- src/components/social-link.tsx
+++ src/components/social-link.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function SocialLink({ url, icon, name }: { url: string, icon: React.ReactNode, name: string }) {
 	return (
-		<Link href={url} className="rounded-full border flex items-center gap-1 p-2 border-neutral-400">
+		<Link href={url} target="_blank" className="rounded-full border flex items-center gap-1 p-2 border-neutral-400 hover:bg-slate-800 hover:text-neutral-100 transition-all">
 			{icon} {name}
 		</Link>
 	)
